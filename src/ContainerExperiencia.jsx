512e4c006b2470cec6ceadba4712f01d79dd5945
dev <JorgePrograma>
2025-01-20 18:11:31 -0500

    commit prueba 2025

--- src/ContainerExperiencia.jsx
+++ src/ContainerExperiencia.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ContainerExperiencia = () => {
   return (
-    <div className='text-sm text-slate-700/100 text-justify'>A lo largo de mi trayectoria profesional, he centrado mi energía y creatividad en la ejecución de diversos proyectos personales que han sido fundamentales para mi desarrollo como desarrollador full stack. Estos proyectos no solo me han proporcionado la oportunidad de aplicar y fortalecer mis habilidades en tecnologías de vanguardia, sino que también me han permitido abordar desafíos complejos tanto en el backend como en el frontend.</div>
+    <div className='text-sm text-slate-700/100 text-justify'>A lo largo de mi trayectoria profesional,  en parking internationa he centrado mi energía y creatividad en la ejecución de diversos proyectos personales que han sido fundamentales para mi desarrollo como desarrollador full stack. Estos proyectos no solo me han proporcionado la oportunidad de aplicar y fortalecer mis habilidades en tecnologías de vanguardia, sino que también me han permitido abordar desafíos complejos tanto en el backend como en el frontend.</div>
   )
 }
 
